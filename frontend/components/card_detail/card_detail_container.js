6b9c95a1a1cfc1286539b4d741a4523e70e089d6
Rob Lim <robert.m.lim@gmail.com>
2017-09-25 19:22:54 -0700

    Implement card detail container

---
+++ frontend/components/card_detail/card_detail_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { fetchCard } from '../../actions/card_actions';
+import CardDetail from './card_detail';
+
+const mapStateToProps(state, ownProps) => ({
+  card: ownProps.card,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCard: (cardId) => dispatch(fetchCard(cardId))
+});
+
+const CardDetailContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CardDetail);
+
+export default CardDetailContainer;
