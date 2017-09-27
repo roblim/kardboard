d8f528cdf9db2d5d180221553c82cc37b2f1c97b
Rob Lim <robert.m.lim@gmail.com>
2017-09-27 12:08:13 -0700

    Implement comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,39 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(card_id: params[:card_id])
+    render :index
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 400
+    end
+  end
+
+  def update
+    @comment = Comment.find_by_id(params[:id])
+    render json: ["Comment does not exist"], status: 404 and return unless @comment
+    @comment.update_attributes(card_params)
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 400
+    end
+  end
+
+  def destroy
+    @comment = Card.find_by_id(params[:id])
+    render json: ["Card does not exist"], status: 404 and return unless @comment
+    @comment = @comment.destroy
+    render :show
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :card_id, :author_id, :created_at, :updated_at)
+  end
+end
