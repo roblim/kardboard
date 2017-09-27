b811263d30ce5e0cc24b50c4e198736ce593ae62
Rob Lim <robert.m.lim@gmail.com>
2017-09-27 11:26:10 -0700

    Implement comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,4 +11,13 @@
 #
 
 class Comment < ApplicationRecord
+  validates :body, :card_id, :author_id, presence: true
+
+  belongs_to :author,
+  foreign_key: :author_id,
+  primary_key: :id,
+  class_name: :User
+
+  belong_to :card, dependent: :destroy
+
 end
