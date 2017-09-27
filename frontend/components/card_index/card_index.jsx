9802189d3c2d68d0e9a9747dccfc281a53bb9d68
Rob Lim <robert.m.lim@gmail.com>
2017-09-27 15:47:26 -0700

    Switch over to cloudinary for managing image backgrounds

--- frontend/components/card_index/card_index.jsx
+++ frontend/components/card_index/card_index.jsx
@@ -11,7 +11,8 @@ const customStyles = {
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
+    background            : '#edeff0'
   }
 };
 
--- frontend/components/comment_index/comment_index_item.jsx
+++ frontend/components/comment_index/comment_index_item.jsx
@@ -7,12 +7,14 @@ class CommentIndexItem extends React.Component {
 
   render() {
     return(
-      <div className="comment-index-item">
-        <div className="comment-index-item-body">
-          {this.props.comment.body}
-        </div>
-        <div className="comment-index-item-info">
-          {this.props.comment.createdAt}
+      <div className="comment-index-item-container">
+        <div className="comment-index-item-box">
+          <div className="comment-index-item-body">
+            {this.props.comment.body}
+          </div>
+          <div className="comment-index-item-info">
+            {this.props.comment.createdAt}
+          </div>
         </div>
       </div>
     )
